833cf221ae0be54750a141390ec7dcc817209140
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2024-07-10 16:37:38 +0100

    Copy file data to user shared memory instead of P variable

--- read_csv.c
+++ read_csv.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	InitLibrary();  // Required for accessing Power PMAC library
+	double exec_time = GetCPUClock();
 
 	char *filename = "/var/ftp/csv_test.csv";
     FILE *file = fopen(filename, "r");
@@ -17,14 +18,28 @@ int main(void)
         printf("Could not open file %s", filename);
         return -1;
     }
+	
+    // USHM buffers
+    int *pushm_time;
+    int *pushm_user;
+    double *pushm_positions[9];
+    double *pushm_velocities[9];
 
+    pushm_time = (int *) pushm + 1000;
+    pushm_user = (int *) pushm + 1001;
+    
+    int axis;
+    for (axis = 0; axis < 9; axis++) {
+        pushm_positions[axis] = (double *)(pushm + 501 + axis);
+    }
+    
+    for (axis = 0; axis < 9; axis++) {
+        pushm_velocities[axis] = (double *)(pushm + 510 + axis);
+    }
 
     char line[MAX_LINE_SIZE];
-	int line_count, column_count;
-
-	double exec_time = GetCPUClock();
-	int axis;
-	line_count=0;
+	int line_count;
+    line_count=0;
 
     // Read the first line
     if (fgets(line, sizeof(line), file) != NULL) {
@@ -36,19 +51,30 @@ int main(void)
         }
     }
 
-	column_count=0;
-    while (fgets(line, MAX_LINE_SIZE, file)) {
+    #define NUM_FIELDS 19 // Actually is 20, but the increment is 19 because time and user are integers
+    while (fgets(line, sizeof(line), file)) {
         char *field = strtok(line, ",");
-        column_count=0;
-        while (field) {
-            pshm->P[(int)(line_count+column_count*1000)]=atof(field);
-            printf("P[%d]=%s ", (int)(line_count+column_count*1000),field);
+        
+        *pushm_time = atoi(field);
+        field = strtok(NULL, ",");
+        pushm_time += NUM_FIELDS;
+        *pushm_user = atoi(field);
+        field = strtok(NULL, ",");
+        pushm_user += NUM_FIELDS;
+
+        for (axis = 0; axis < 9; axis++) {
+            *pushm_positions[axis] = atof(field);
             field = strtok(NULL, ",");
-			column_count++;
-	    }
-		line_count++;
-//        printf("\n");
+            pushm_positions[axis] += NUM_FIELDS;
+        }
+        for (axis = 0; axis < 9; axis++) {
+            *pushm_velocities[axis] = atof(field);
+            field = strtok(NULL, ",");
+            pushm_velocities[axis] += NUM_FIELDS;
+        }
+        line_count++;
     }
+        
     fclose(file);
 	exec_time = GetCPUClock()-exec_time;
 	printf("Lines number: %d\n", line_count);
@@ -56,3 +82,4 @@ int main(void)
 	CloseLibrary();
 
 	return 0;
+}
