b82e5fd15aac97abf106d55da857bd6d74cf35fb
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2024-07-11 16:52:01 +0100

    Fix typo in definitions

--- read_csv.h
+++ read_csv.h
@@ -6,6 +6,6 @@
 #define USHM_BASE_ADDR              8
 #define USHM_INT_BASE_IDX           (int)(USHM_BASE_ADDR/sizeof(int))
 #define USHM_DOUBLE_BASE_IDX        (int)(USHM_INT_BASE_IDX + 2)*sizeof(int)/sizeof(double)
-#define USHM_OFFSET_ADDR            2*sizeof(int) + 2*AXES_NUMBER*sizeof(double)
+#define USHM_OFFSET_ADDR            2*sizeof(int) + 2*NUM_AXES*sizeof(double)
 #define USHM_OFFSET_INT_IDX         USHM_OFFSET_ADDR/sizeof(int)
 #define USHM_OFFSET_DOUBLE_IDX      USHM_OFFSET_ADDR/sizeof(double)
