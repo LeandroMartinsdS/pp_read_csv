779567c53f771e15bd4b0deea49b600a5dd5e230
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2024-11-27 17:06:57 +0000

    Parametrize MAX_LINE_SIZE and fix division

--- read_csv.h
+++ read_csv.h
@@ -1,15 +1,18 @@
 // File definitions
-#define MAX_LINE_SIZE   1024
-#define MAX_LINES       1000
-#define NUM_AXES        9
 
+#define MAX_LINES       1001
+#define NUM_AXES        8
+#define INT_CHARS       2*11
+#define DOUBLE_CHARS    NUM_AXES*25
+#define COMMA_CHARS     NUM_AXES+2
+#define MAX_LINE_SIZE   INT_CHARS+DOUBLE_CHARS+COMMA_CHARS+1
 
 // Buffer definitions
 // Base Address
 #define USHM_BASE_ADDR              4000
 // Base indices
 #define USHM_INT_BASE_IDX           (int)(USHM_BASE_ADDR/sizeof(int))
-#define USHM_DOUBLE_BASE_IDX        (int)((USHM_INT_BASE_IDX+ 2)*sizeof(int)/sizeof(double))
+#define USHM_DOUBLE_BASE_IDX        (int)((USHM_INT_BASE_IDX+ 2)/(sizeof(double)/sizeof(int)))
 
 #define USHM_LINE_OFFSET_ADDR            (int)(2*sizeof(int)+2*NUM_AXES*sizeof(double))
 #define USHM_LINE_OFFSET_INT_IDX         (int)(USHM_LINE_OFFSET_ADDR/sizeof(int))
