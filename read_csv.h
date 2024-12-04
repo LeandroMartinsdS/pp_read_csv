2c2f1ba6b8398879e969dee913db811345bbcf2c
Leandro Martins dos Santos <Leandro.Martins-dos-Santos@diamond.ac.uk>
2024-12-04 17:05:52 +0000

    Change macro definitions for RtTraj format instead of PVT

--- read_csv.h
+++ read_csv.h
@@ -3,8 +3,8 @@
 #define MAX_LINES       1001
 #define NUM_AXES        8
 #define INT_CHARS       2*11
-#define DOUBLE_CHARS    NUM_AXES*25
-#define COMMA_CHARS     NUM_AXES+2
+#define DOUBLE_CHARS    NUM_AXES*25 // 2*NUM_AXES*25  for PVT
+#define COMMA_CHARS     NUM_AXES+2  // 2*NUM_AXES+2 for PVT
 #define MAX_LINE_SIZE   INT_CHARS+DOUBLE_CHARS+COMMA_CHARS+1
 
 // Buffer definitions
@@ -14,10 +14,11 @@
 #define USHM_INT_BASE_IDX           (int)(USHM_BASE_ADDR/sizeof(int))
 #define USHM_DOUBLE_BASE_IDX        (int)((USHM_INT_BASE_IDX+ 2)/(sizeof(double)/sizeof(int)))
 
-#define USHM_LINE_OFFSET_ADDR            (int)(2*sizeof(int)+2*NUM_AXES*sizeof(double))
+// 2*NUM_AXES is only when writing PVT
+//#define USHM_LINE_OFFSET_ADDR            (int)(2*sizeof(int)+2*NUM_AXES*sizeof(double))
+#define USHM_LINE_OFFSET_ADDR            (int)(2*sizeof(int)+NUM_AXES*sizeof(double))
 #define USHM_LINE_OFFSET_INT_IDX         (int)(USHM_LINE_OFFSET_ADDR/sizeof(int))
 #define USHM_LINE_OFFSET_DOUBLE_IDX      (int)(USHM_LINE_OFFSET_ADDR/sizeof(double))
-
 
 #define USHM_BUFF_SIZE                   1000
 
